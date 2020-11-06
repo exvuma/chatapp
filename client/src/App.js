dbf663b7ed54e946006a5356dbef16e1e0ec08d7
Victoria <Bernard.victoria22@gmail.com>
2020-11-06 15:36:25 -0600

    Fix sign in styles

--- client/src/App.js
+++ client/src/App.js
@@ -25,21 +25,30 @@ import { MOCK_ROOMS } from './mocks';
 const SignInCard = props => {
   const { setIsEditingName, name, setName, fetchRooms } = props;
   return (
-    <Box className='App-intro'>
-      <AppBar className='makeStyles-appBar-2'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={'classes.menuButton'}
-            color='inherit'
-            aria-label='menu'
-          ></IconButton>
-          <Typography variant='h6' noWrap>
-            Welcome
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box m={'3rem'} alignItems='center' justifyContent='center'>
+    <Box
+      className='App-intro'
+      style={{
+        margin: '3rem',
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Toolbar>
+        <Typography variant='h6' noWrap>
+          Welcome to Chat App
+        </Typography>
+      </Toolbar>
+      <Box
+        style={{
+          flex: 1,
+          margin: '3rem',
+          justifyContent: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
         <InputLabel>What's your name ?</InputLabel>
         <form
           onSubmit={e => {
@@ -47,16 +56,30 @@ const SignInCard = props => {
             setIsEditingName(false);
             fetchRooms();
           }}
+          style={{
+            flex: 1,
+          }}
           noValidate
           autoComplete='off'
         >
           <TextField
+            style={{
+              flex: 1,
+            }}
             label={'My Name'}
             id='standard-basic'
             value={name}
             onChange={e => setName(e.target.value)}
           />
-          <Button>Submit</Button>
+          <Button
+            color='primary'
+            style={{
+              flex: 1,
+              margin: '1rem',
+            }}
+          >
+            Submit
+          </Button>
         </form>
       </Box>
     </Box>
