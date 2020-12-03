6663511758de134d2e16c264205beb6524b0ace6
Victoria <Bernard.victoria22@gmail.com>
2020-12-03 13:51:08 -0600

    remove console logs

--- server/api.js
+++ server/api.js
@@ -69,7 +69,6 @@ const appendMsgs = msg => {
 };
 const appendRooms = room => {
   //TODO validate room type
-
   rooms = [...rooms, room];
   return rooms;
 };
@@ -84,19 +83,13 @@ const appendUsers = name => {
 };
 
 app.get('/*.html', (req, res) => {
-  //   res.sendFile(__dirname + "../build/index.html");
-  res.sendFile(
-    '/Users/victoriabernard/my-repos/raise-interview/build/index.html'
-  );
+  res.sendFile(__dirname + '../build/index.html');
 });
 app.get('/socket.io', (req, res) => {
-  console.log('get msg from socket.io');
   io.emit('FromAPI', JSON.stringify(messages));
   res.end(JSON.stringify(messages), 200);
 });
 app.post('/api/messages', (req, res) => {
-  console.log('api, ', req.body);
-  // io.emit("FromAPI", 22);
   io.emit('FromAPI', JSON.stringify(req.body));
   messages = appendMsgs(req.body);
   res.send(JSON.stringify(messages), 200);
@@ -108,7 +101,6 @@ app.get('/api/messages', (req, res) => {
   res.send(JSON.stringify(messages), 200);
 });
 app.post('/api/rooms', (req, res) => {
-  // io.emit("FromAPI", 22);
   io.emit('POST_ROOM', JSON.stringify(req.body));
   rooms = appendRooms(req.body);
   res.send(JSON.stringify(rooms), 200);
@@ -117,32 +109,24 @@ app.get('/api/rooms', (req, res) => {
   res.send(JSON.stringify(rooms), 200);
 });
 app.get('/api/rooms/:id/messages', (req, res) => {
-  console.log(req.params.id);
   const roomId = req.params.id;
   const roomMsgs = messages.filter(msg => msg.roomId === roomId);
   res.send(JSON.stringify(roomMsgs), 200);
 });
 io.on('connection', socket => {
-  // console.log("a user connected");
   io.emit('NewUser');
   socket.broadcast.emit('NewUser');
-  // socket.on("disconnect", (reason) => {
-  // console.log("user disconnected", reason);
-  // });
   socket.on('NewRoom', data => {
     rooms = appendRooms(data);
     io.emit('RoomCreated', JSON.stringify(rooms));
     socket.broadcast.emit('RoomCreated', JSON.stringify(rooms));
   });
   socket.on('NewMessage', data => {
-    console.log('NewMessage data', data);
     messages = appendMsgs(data);
     io.emit('MsgCreated', JSON.stringify(messages));
     socket.broadcast.emit('MsgCreated', JSON.stringify(messages));
-    console.log('succccess on emitted', messages);
   });
 });
-// module.exports.handler = serverless(app);
 http.listen(PORT, () => {
   console.log('listening on *:' + PORT);
 });
